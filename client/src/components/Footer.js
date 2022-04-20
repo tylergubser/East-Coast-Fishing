1097323ff26e4e66dd0f11d32ca341221f48eeaa
Vi Vo <aaronvo118@gmail.com>
2022-04-20 17:07:37 -0500

    restyle of profile

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
         <div class="max-w-3xl mx-auto space-y-6">
           <div className="text-center font-light font-sm">
             <a href="https://www.linkedin.com/in/v12v0/" className="hover:text-blue-700"> Vi V. | </a>
-            <a href="https://www.linkedin.com/in/stephen-talley-the-developer/" className="hover:text-blue-700" > Stephen T. | </a>
+            <a href="http://www.linkedin.com/in/tyler-gubser" className="hover:text-blue-700"> Tyler G. | </a>
             <a href="https://www.linkedin.com/in/echernicky/" className="hover:text-blue-700"> Evan C. | </a>
-            <a href="http://www.linkedin.com/in/tyler-gubser" className="hover:text-blue-700"> Tyler G.</a>
+            <a href="https://www.linkedin.com/in/stephen-talley-the-developer/" className="hover:text-blue-700" > Stephen T.</a>
             <p>2022</p>
           </div>
         </div>
