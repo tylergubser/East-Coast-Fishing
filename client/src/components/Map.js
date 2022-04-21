ddb99fbdbfcd3b739946707cb47892a0bdc4abed
Stephen Talley <stvlley.amzn@gmail.com>
2022-04-21 00:11:34 -0400

    did the map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-function Map() {
-  return (
-    <div>map</div>
-  )
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 40.704755,
+      lng: -74.014738
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBeFIrVowM6TweV58EWcLV-iRMJ-E4NApA' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={40.704755}
+            lng={-74.014738}
+            text="Flation School"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
 
-export default Map
+export default Map;
